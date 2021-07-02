d3f3fe69e3b27cf73ba7f49bf911fd0eeb4544c5
Trevor Leffert <leffertt@gmail.com>
2021-07-02 02:04:55 -0500

    last bit of fun, added no result state

--- src/components/dog-search-results/DogSearchResults.jsx
+++ src/components/dog-search-results/DogSearchResults.jsx
@@ -8,19 +8,29 @@ import DogTile from '../dog-tile/DogTile';
 
 const DogSearchResults = (props) => {
 
+
+    const buildResults = (results) => {
+        return results.map(dogger => (
+            <Col className="mt-3" lg="3" md="4" sm="6" key={dogger.breed}>
+                <LazyLoad>
+                    <DogTile dogger={dogger}></DogTile>
+                </LazyLoad>
+            </Col>
+        ));
+    }
+
     return (
         <Container>
-            <CardDeck>
             {
-                props.results.map(dogger => (
-                    <Col className="mt-3" lg="3" md="4" sm="6" key={dogger.breed}>
-                        <LazyLoad>
-                            <DogTile dogger={dogger}></DogTile>
-                        </LazyLoad>
-                    </Col>
-                ))
+                props.results.length ?
+                <CardDeck>
+                {
+                    buildResults(props.results)
+                }
+                </CardDeck>
+                : <p className="text-center">Who let the dogs out? Because they're all gone! (who, who)</p>
             }
-            </CardDeck>
+
         </Container>
     );
 }
