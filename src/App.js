f13cb22d6a90755eb2524830028ba9c2558e8931
Trevor Leffert <leffertt@gmail.com>
2021-07-01 22:04:13 -0500

    init app setup for fething dogs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+import { useState, useEffect } from 'react';
+
+import Container from 'react-bootstrap/Container';
+
+import { listBreads } from './shared/api/dogApi';
+
+import DogSearch from './components/dog-search/DogSearch';
+import DogSearchResults from './components/dog-search-results/DogSearchResults';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [allBreeds, setAllBreeds] = useState([]);
+    const [searchResults, setSearchResults] = useState([]);
+
+    useEffect(() => {
+        listBreads().then(data => {
+            let breeds = [];
+            let breedData = data.data.message;
+            for (const breed in breedData) {
+                breeds.push({
+                    breed: breed,
+                    subbreeds: breeds[breed]
+                })
+            }
+            setAllBreeds(breeds);
+        })
+    },[]);
+
+    const handleSearchUpdate = (search) => {
+        setSearchResults(allBreeds.filter(dogger => dogger.breed.includes(search)));
+    }
+
+
+    return (
+      <Container>
+        <DogSearch updateSearchTerm={handleSearchUpdate}></DogSearch>
+        <DogSearchResults results={searchResults}></DogSearchResults>
+      </Container>
+    );
 }
 
 export default App;
