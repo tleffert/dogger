77103b8a12fe17c0b948d3b261a774d784a2c3c3
Trevor Leffert <leffertt@gmail.com>
2021-07-02 00:05:36 -0500

    Updated dog tile with better styling for breed overlay on image as well as added subbreed footer bages

--- src/components/dog-tile/DogTile.jsx
+++ src/components/dog-tile/DogTile.jsx
@@ -1,19 +1,66 @@
-import { useState } from 'react';
+import { useState, useEffect, Suspense, lazy, Fragment } from 'react';
+import LazyLoad from 'react-lazy-load';
 
 import Card from 'react-bootstrap/Card';
+import Badge from 'react-bootstrap/Badge';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+
+
+import { getBreedImage } from '../../shared/api/dogApi';
+
+import styles from './DogTile.module.scss';
 
 const DogTile = (props) => {
 
     const [dogger, setDogger] = useState('');
+    const [breedImg, setBreedImg] = useState('');
+
+    useEffect(() => {
+        getBreedImage(props.dogger.breed).then(data => {
+            setBreedImg(data.data.message);
+        });
+    },[])
+
+    // Helper to generate the subbreeds footer if needed.
+    const generateSubBreedBadges = (subbreeds) => {
+            let subbreedBadges = subbreeds.map(subbreed => (
+                <Badge
+                    className="mr-1"
+                    key={props.dogger.breed+'-'+subbreed}
+                    variant="secondary"
+                >
+                    {subbreed}
+                </Badge>
+            ));
+
+            return (
+                <Fragment>
+                    <div>Subbreeds:</div>
+                    {subbreedBadges}
+                </Fragment>
+            );
+    }
 
     return (
-        <Card>
-            <Card.Img src="https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg"/>
+        <Card className={styles.DogTile}>
+            {//<Card.Header>{props.dogger.breed}</Card.Header>
+            }
+            <Card.Img src={breedImg} fluid/>
             <Card.ImgOverlay>
                 <Card.Title>
-                    {props.dogger.breed}
-                </Card.Title>
+                    <Badge variant="dark">{props.dogger.breed}</Badge></Card.Title>
             </Card.ImgOverlay>
+            {
+                props.dogger.subbreeds.length ?
+                    <Card.Footer>
+                        {
+                            generateSubBreedBadges(props.dogger.subbreeds)
+
+                        }
+                    </Card.Footer>
+                : null
+            }
         </Card>
     );
 
