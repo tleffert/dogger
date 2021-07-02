fe0e344053ff1e0cf6be44f4029318223ed7ff3c
Trevor Leffert <leffertt@gmail.com>
2021-07-02 01:20:01 -0500

    cleanup

--- src/components/dog-search/DogSearch.jsx
+++ src/components/dog-search/DogSearch.jsx
@@ -1,20 +1,13 @@
-import { useState, useEffect } from 'react';
-
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
 
 const DogSearch = (props) => {
-    const [search, setSearch] = useState('');
-
-    useEffect(() => {
-        props.updateSearchTerm(search);
-    }, [search])
 
     return (
         <InputGroup>
             <FormControl
                 placeholder="Doggo Breed"
-                onChange={(event) => setSearch(event.target.value)}
+                onChange={(event) => props.updateSearchTerm(event.target.value)}
             />
         </InputGroup>
     );
